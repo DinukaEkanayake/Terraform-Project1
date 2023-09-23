432721461fd92f40d071a889a74fff44d5a6e809
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-09-23 10:49:38 +0530

    create variables

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+variable "ami_value" {
+  description = "value for ami"
+}
+variable "instance_type_value" {
+  description = "value for instance type"
+}
+variable "subnet_id_value" {
+  description = "value for subnet id"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
