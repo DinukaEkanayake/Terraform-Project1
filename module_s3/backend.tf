75b56c47cd40fc681405ebd39565c3347ad9cf8c
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-09-25 21:26:56 +0530

    delete backend files

--- module_s3/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "project1-s3-bucket"
-    key = "terraform/project1/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
